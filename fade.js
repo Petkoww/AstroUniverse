d771d97be7df16f7c607df4912ed442dc4dc8bc8
Ivaylo Petkov <116186028+Petkoww@users.noreply.github.com>
2023-04-25 12:51:50 +0300

    Version 3.0
    Updated version of the site. Edited and connected all the pages. Ready for publishing.

---
+++ fade.js
@@ -0,0 +1,20 @@
+const images = document.querySelectorAll('.fade-in');
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.animationPlayState = 'running';
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+images.forEach(image => {
+  observer.observe(image);
+});
